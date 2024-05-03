3dc19c531b8539e570576990fa7c3d87ec7ff28f
ct_code <77695884+c-tapp@users.noreply.github.com>
2024-05-03 14:21:42 -0400

    Initial commit

---
+++ src/main/java/com/cinemaDB/backend/CinemaDatabaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cinemaDB.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaDatabaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaDatabaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinemaDB/backend/CinemaDatabaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemaDB.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaDatabaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
